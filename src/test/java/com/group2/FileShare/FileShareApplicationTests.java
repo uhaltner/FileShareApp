3b0d529f6af46f3e77b8e48daaec7ed1c645a776
unknown <modi.yash2012@gmail.com>
2019-05-31 12:52:09 -0300

    #1 Initialize Project

---
+++ src/test/java/com/group2/FileShare/FileShareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.group2.FileShare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileShareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
