b30c6a2050e691a2c8b8ed3e09be0cd58e955e71
Ueli Haltner <ul528383@dal.ca>
2019-06-01 16:34:55 -0300

    [JenkinsJUnitTest] Fix for User JUnit test class

--- src/test/java/com/group2/FileShare/UserTest.java
+++ src/test/java/com/group2/FileShare/UserTest.java
@@ -5,10 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootApplication
+@SpringBootTest
 public class UserTest {
 
 	@Test
