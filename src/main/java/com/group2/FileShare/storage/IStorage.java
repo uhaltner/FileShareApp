a307dd00eaa7cb2eb721d5a5478967b720c017ef
Samson Maconi <git@samsonmaconi.com>
2019-06-19 03:10:11 -0300

    [storage] - document storage package implemented - file upload - file download - file delete

--- src/main/java/com/group2/FileShare/storage/IStorage.java
+++ src/main/java/com/group2/FileShare/storage/IStorage.java
@@ -3,6 +3,20 @@ package com.group2.FileShare.storage;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface IStorage {
-	String downloadFile(MultipartFile file);
-	String uploadFile(MultipartFile file);
+	/**
+	 * @param filePath - The path of the file to be deleted
+	 * @return True if successful and False otherwise
+	 */
+	boolean deleteFile(String filePath);
+	/**
+	 * @param filePath - The path of the file to be downloaded
+	 * @return downloadFile URL String
+	 */
+	String downloadFile(String filePath);
+	/**
+	 * @param file - A representation of an uploaded file received in a multipart request.
+	 * @param filename - Name to store the file as.
+	 * @return True if successful and False otherwise
+	 */
+	boolean uploadFile(MultipartFile file, String filename);
 }
