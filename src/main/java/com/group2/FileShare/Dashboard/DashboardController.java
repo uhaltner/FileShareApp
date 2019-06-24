08fc44815df0a4fe6a6610dd33923f4322d53b68
Yash <modi.yash2012@gmail.com>
2019-06-23 22:52:37 -0300

    Implemented upload file modal and updated dashboard controller

--- src/main/java/com/group2/FileShare/Dashboard/DashboardController.java
+++ src/main/java/com/group2/FileShare/Dashboard/DashboardController.java
@@ -1,19 +1,39 @@
 package com.group2.FileShare.Dashboard;
 
-import javax.servlet.http.HttpSession;
-
+import com.group2.FileShare.Authentication.AuthenticationSessionManager;
+import com.group2.FileShare.document.DocumentDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.group2.FileShare.Authentication.AuthenticationSessionManager;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class DashboardController {
 
+    private AuthenticationSessionManager sessionManager;
+    private DocumentDAO documentDAO;
+
     @GetMapping("/dashboard")
-    public String profileForm(HttpSession session, Model model) {
-        return "dashboard";
+    public String profileForm(HttpSession session, ModelAndView modelAndView, Model model)
+    {
+        documentDAO = new DocumentDAO();
+        sessionManager = AuthenticationSessionManager.instance();
+
+        String firstName = sessionManager.getFirstName();
+        String lastName = sessionManager.getLastName();
+        boolean isUserLoggedIn = sessionManager.isUserLoggedIn();
+        //List<Document> documentList = documentDAO.getDocuments();
+        //modelAndView.addObject("documents", documentList);
+        model.addAttribute("firstName",firstName);
+        model.addAttribute("lastName",lastName);
+
+        if (isUserLoggedIn) {
+            return "dashboard";
+        } else {
+            return "landing";
+        }
     }
-    
+
 }
